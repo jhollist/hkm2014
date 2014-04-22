80869805fb45c9acadcbaae289f379f43d07a5c3
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-04-22 16:33:58 -0400

    updated data

--- hkm/R/gisVarSelRF.R
+++ hkm/R/gisVarSelRF.R
@@ -1,9 +1,10 @@
 #' Function to run GIS variables through varSelRF
 #' @export
 #' @examples
+#' data(hkm2014Data)
 #' gis_var<-names(hkm2014Data)[c(4:152,154,194:210)]
-#' gisData<-complete.cases(hkm2014Data[,gis_var])
-#' 
+#' gis_var<-gis_var[-grep("NA",gis_var)]
+#' gisData<-hkm2014Data[,gis_var][complete.cases(hkm2014Data[,gis_var]),]
 #' gisvs<-gisVarSelRF(gisData[,1:164],gisData[,167],ntree = 100, ntreeIterat = 50,vars.drop.frac = 0.1)
 #' gisvs
 #' plot(gisvs)
