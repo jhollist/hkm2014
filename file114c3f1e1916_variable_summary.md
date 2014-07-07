bddceece5214c1b32eda92bb09fe01e0376ee25f
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-07-07 16:12:18 -0400

    updated analysis log

---
+++ file114c3f1e1916_variable_summary.md
@@ -0,0 +1,13 @@
+|   |Variable           | Percent|
+|:--|:------------------|-------:|
+|3  |WSA_ECO9           |     1.0|
+|4  |CropsPer_3000m     |     0.5|
+|1  |EvergreenPer_3000m |     0.4|
+|2  |MeanDepthCorrect   |     0.3|
+|5  |AlbersX            |     0.3|
+|6  |AlbersY            |     0.1|
+|7  |DDs45              |     0.1|
+|8  |ELEV_PT            |     0.1|
+|9  |HerbWetPer_3000m   |     0.1|
+|10 |MaxDepthCorrect    |     0.1|
+|11 |MixedForPer_3000m  |     0.1|
--- iterVarSelRF.R
+++ iterVarSelRF.R
@@ -4,7 +4,9 @@
 #' possibly selected variables.  This process forms the basis of the analysis 
 #' conducted in Hollister, Kreakie, and Milsted (2014).  We us the  proportion 
 #' of iterations that a given variable is selected as a variable selection 
-#' metric. A parallel implementation is possible using `snow`.
+#' metric. A parallel implementation is possible using `snowfall`.  User only need 
+#' specify the number of cores to use.  Function relies on knitr,snowfall,parallel,
+#' and varSelRF
 #' 
 #' @param indVar independent variables
 #' @param depVar dependent variable
@@ -12,20 +14,56 @@
 #' @param numCore number of cores to split runs across. defualt is 1
 #' @param outStr string to append to output files. Defaults to a tmp file
 #' @param ... variables from varselRF  
+#' 
+#' @examples
+#' system.time(iterVarSelRF(Pgis[keep,],TS_CHLA[keep],10,ntree=10,ntreeIterat=5,
+#' vars.drop.frac=NULL,vars.drop.num=1))
+#' system.time(iterVarSelRF(Pgis[keep,],TS_CHLA[keep],10,4,ntree=10,ntreeIterat=5,
+#' vars.drop.frac=NULL,vars.drop.num=1))
+#' @export
 
-iterVarSelRF<-function(indVar,depVar,numRun=10,numCore=1,outStr=tempfile(),...){
+iterVarSelRF<-function(indVar,depVar,numRun=10,numCore=1,outStr=tempfile(tmpdir=getwd()),
+											 ntree=10,ntreeIterat=5,vars.drop.frac=NULL,vars.drop.num=1){
 	
-	#Add parallell
-	#Add saving intermediate
-	#Add summary
-	#Add return list of output all selected, summary, and params
-	Out<-vector("list",numRun)
-	for(i in 1:numRun){
-		rfGIS<-varSelRF(indVar,depVar,...)
-		Out[[i]]<-rfGIS$selected.vars
-	
-		lapply(Out,write,"gis_selected_variables_ts.csv",append=T,ncolumns=1000)
-		print(i)
+	#validate number of cores
+	if(numCore>parallel::detectCores()){
+		return(stop(paste("Your system only has",detectCores(),"cores available")))
 	}
 	
+	#Set up single core/sequential
+	if(numCore==1){
+		sfInit(parallel=FALSE)
+	}
+	
+	#Set up multiple cores/parallel
+	if(numCore>1){
+		sfInit(parallel=TRUE,cpus=numCore)
+		sfExport(list=c("indVar","depVar","ntree","ntreeIterat","vars.drop.frac",
+										"vars.drop.num"), local=TRUE)
+		sfLibrary(varSelRF)
+	}
+	
+	#varSelRF Function to pass to sfLapply
+	sfVarSelRF<-function(x){
+		outVarSel<-varSelRF(indVar,depVar,ntree=ntree,ntreeIterat=ntreeIterat,
+												vars.drop.frac=vars.drop.frac,
+												vars.drop.num=vars.drop.num)
+  	return(outVarSel$selected.vars)
+	}
+	
+	#Run it
+	result <- sfLapply(1:numRun,sfVarSelRF)
+	
+	#summarize variables
+	summDF<-data.frame()
+	uniqueVar<-unique(unlist(result))
+	for(i in uniqueVar){
+		summDF<-rbind(summDF,data.frame(Variable=i,Percent=sum(unlist(result)%in%i)/numRun))
+	}
+	summDF<-summDF[order(summDF$Percent,decreasing=T),]
+	write(kable(summDF),paste(outStr,"_variable_summary.md",sep=""))
+	#Add return list of output all selected, summary, and params
+	lapply(result,write,paste(outStr,"_selected_variables_ts.csv",sep=""),append=T,ncolumns=1000)
+	sfStop()
+  return(result)
 }
